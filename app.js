3d23b72ea2646af51faa56aec78f976d538e127d
lals80 <lals8086@gmail.com>
2020-05-03 22:47:12 +0900

    frontEnd 작업

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: false }));
-
 const gameRouter = require('./Router/GameRouter');
 app.use(gameRouter);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/gameList.html');
+});
+app.get('/gameAdd.html', (req, res) => {
+    res.sendFile(__dirname + '/views/gameAdd.html');
+});
+app.get('/gameInfo.html', (req, res) => {
+    res.sendFile(__dirname + '/views/gameInfo.html');
+});
 
 module.exports = app;
