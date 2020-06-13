79983b246802d888f582eee3a01853b421711b37
lals80 <lals8086@gmail.com>
2020-06-14 02:00:07 +0900

    누락추가

--- Router/GameRouter.js
+++ Router/GameRouter.js
@@ -27,17 +27,36 @@ const user = {
 function handleLogin(req, res) {
     const id = req.body.id;
     const pw = req.body.password;
-
+ 
     // 로그인 성공
-    if (id === user.id && pw === user.password) {
-        // 토큰 생성
-        const token = jwt.sign({ id: user.id, name: user.name }, secretKey);
-        res.send({ msg: 'success', token: token });
+    if ( id === user.id && pw === user.password ) {
+       // 토큰 생성
+       const token = jwt.sign( { id:user.id, name:user.name }, secretKey );
+       res.send({msg:'success', token:token});
     }
     else {
-        res.sendStatus(401);
+       res.sendStatus(401);
     }
-}
+ }
+ 
+ function sendPublicInfo(req, res) {
+    res.send({msg : 'This is public information'});
+ }
+ 
+ // 프로필 보기 - 인증된 사용자만 가능
+ function sendProfile(req, res) {
+    // 요청 헤더 중 Authorization 필드로 토큰 얻기
+    const token = req.headers['authorization'];
+    console.log('token :', token);
+    jwt.verify(token, secretKey, (err, decoded) => {
+       if ( err ) {
+          res.sendStatus(401);
+          return;
+       }
+       res.send({id: user.id, name: user.name});
+    }); 
+ }
+
 
 async function showGameList(req, res) {
     const gameList = await games.getGameList();
